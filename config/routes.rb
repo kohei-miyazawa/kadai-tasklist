9f983a1c5a9c1cb160e671e6c1e7a18cff93c6ca
kohei.miyazawa <kohei.miyazawa120@gmail.com>
2021-02-03 06:45:34 +0000

    user login

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,15 @@
 Rails.application.routes.draw do
-  get 'users/new'
-  get 'users/create'
+  get 'sessions/new'
+  get 'sessions/create'
+  get 'sessions/destroy'
   root to: 'tasks#index'
   resources :tasks
   
-   get 'signup', to: 'users#new'
+  get 'signup', to: 'users#new'
+   
+  get 'login', to: 'sessions#new'
+  post 'login', to: 'sessions#create'
+  delete 'logout', to: 'sessions#destroy'
+  
    resources :users, only: [:create]
 end
